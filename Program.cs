ea133e454b1a643103b12d0d207612f55e84cfaa
Sergey <xxsmotur@gmail.com>
2018-12-14 18:25:09 +0500

    Moving argument parser to separate class

--- Program.cs
+++ Program.cs
@@ -1,24 +1,14 @@
 ﻿using System;
 using YaTranslate.Core;
-using CommandLine;
 
 namespace YaTranslate
 {
-    public class Options
-    {
-        [Option('h', "help", Required = false, HelpText = "Showing of help info")]
-        public bool Verbose { get; set; }
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0) {
-                System.Console.WriteLine("Please enter arguments");
-                return;
-            }
-            using (ProgramCore core = new ProgramCore())
+            using (ProgramCore core = new ProgramCore(args))
             {
                 core.Execute();
             }
