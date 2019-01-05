55640f937fab41042b85c18c85c8f629dc4b47ed
Sergey <xxsmotur@gmail.com>
2019-01-05 22:53:49 +0500

    Implicit operatior of init settings

--- src/YaTransalate/Services/Settings.cs
+++ src/YaTransalate/Services/Settings.cs
@@ -2,5 +2,9 @@ namespace YaTranslate.Services {
 
     public struct Settings {
         public string Token {get;set;}
+
+        public static implicit operator Settings(string token) {
+            return new Settings() { Token = token };
+        }
     } 
 }
