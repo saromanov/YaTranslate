9ae59e0d7d110d4ccde8be260cbf87095c93628a
Sergey <xxsmotur@gmail.com>
2019-01-05 23:41:35 +0500

    Add token key as required attribute

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace YaTranslate
     {
         [Option('l', "languages", Required = false, HelpText = "Showing if list supported languages")]
         public bool Languages { get; set; }
+
+        [Option('t', "token", Required = true, HelpText = "Token of the app")]
+        public string Token { get; set; }
     }
 
     class Program
@@ -21,6 +24,7 @@ namespace YaTranslate
                    {
                        if (o.Languages)
                        {
+                           Console.WriteLine(o.Token);
                            core.Languages();
                        }
                    });
